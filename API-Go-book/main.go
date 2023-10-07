5931fe7e5e00555bc93a9d5c1eb0bff496386911
zikrillah <dzikry.aa@gmail.com>
2023-10-07 19:51:08 +0700

    finish route for post book

--- API-Go-book/main.go
+++ API-Go-book/main.go
@@ -24,8 +24,20 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
